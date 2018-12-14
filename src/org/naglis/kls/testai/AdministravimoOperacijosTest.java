8a08dd7c058dabb21a68632ed130f9eb2ac94347
naglis11 <45588003+naglis11@users.noreply.github.com>
2018-12-14 13:29:59 +0200

    final3

--- src/org/naglis/kls/testai/AdministravimoOperacijosTest.java
+++ src/org/naglis/kls/testai/AdministravimoOperacijosTest.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+import org.naglis.kls.data.IData;
 import org.naglis.kls.modelis.Klientas;
 import org.naglis.kls.procesai.AdministravimoOperacijos;
 import org.naglis.kls.procesai.PaieskosKriterijai;
@@ -14,25 +15,29 @@ public class AdministravimoOperacijosTest {
 
 	@Test
 	public void testPaieska() {
-		
-		AdministravimoOperacijos underTest = new AdministravimoOperacijos();
 
-		List<Klientas> bandomasis = new ArrayList<>();
-		
-		bandomasis.add(new Klientas("Tomas", "Malinauskas", "V", "1985.02.04", "86000000", "3"));
-		
+		AdministravimoOperacijos underTest = new AdministravimoOperacijos(new IData() {
+
+			@Override
+			public List<Klientas> bazesPapildymas() {
+				List<Klientas> bandomasis = new ArrayList<>();
+
+				bandomasis.add(new Klientas("v1", "p1", "V", "1985.02.04", "111", "3"));
+				bandomasis.add(new Klientas("v2", "p2", "V", "1985.02.04", "111", "3"));
+				return bandomasis;
+			}
+		});
+
 		PaieskosKriterijai kriterijus = new PaieskosKriterijai();
 		kriterijus.setaVardas(true);
-		kriterijus.setVardas("Tomas");
-		
+		kriterijus.setVardas("v2");
+
 		List<Klientas> rezultatas = underTest.paieskosKriterijausParinkimas(kriterijus);
-		
-		boolean arLygu = false;
-		
-		arLygu = bandomasis.toString().contentEquals(rezultatas.toString());
-		
-		assertEquals(arLygu, true);
-				
+
+		assertEquals(1, rezultatas.size());
+
+		assertEquals("p2", rezultatas.get(0).getPavarde());
+
 	}
-	
+
 }
